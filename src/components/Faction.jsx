799b96940a0387b78d92f26c2b53c30003c5bfdb
Bryan <bryan@MacBook-Air-de-Bryan.local>
2024-04-02 16:50:10 -0600

    add faction section

--- src/components/Faction.jsx
+++ src/components/Faction.jsx
@@ -1,14 +1,52 @@
-
+import Gigerianus from '../assets/gigerianus.webp'
+import Human from '../assets/human.webp'
+import Venyator from '../assets/veniator.webp'
+import Citadel from '../assets/citadel.webp'
+import Staynetech from '../assets/staynetech.webp'
+import Mercenarios from '../assets/merc.webp'
 
 const Faction = () => {
   return (
-    <div className="w-full h-fit p-8 lg:p-36 md:p-24">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className='flex w-full h-fit flex-col justify-center items-center'>
+    <h2 className='font-bold text-3xl mb-10 lg:mb-0 md:mb-0 lg:text-5xl text-white'>Facciones</h2>
+    <div className="w-full h-fit px-8 lg:px-36 md:p-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <a href='#' className='faction-card hover:shadow-[0px_0px_89px_0px_#4c51bf] hover:shadow-[0px_0px_89px_0px_#4c51bf]'>
+        <div className='faction-blur'>
+          <p>Gigerianus</p>
+        </div>
+        <img src={Gigerianus} alt="" />
+      </a>
+      <a href='' className='faction-card hover:shadow-[0px_0px_89px_0px_#D9002E]'>
+        <div className='faction-blur'>
+          <p>Fuerza de Defensa Humana</p>
+        </div>
+        <img src={Human} alt="" />
+      </a>
+      <a href='' className='faction-card hover:shadow-[0px_0px_89px_0px_#fff]'>
+        <div className='faction-blur'>
+          <p>Venyator</p>
+        </div>
+        <img src={Venyator} alt="" />
+      </a>
+      <a href='' className='faction-card hover:shadow-[0px_0px_89px_0px_#007AD9] '>
+        <div className='faction-blur'>
+          <p>Citadel</p>
+        </div>
+        <img src={Citadel} alt="" />
+      </a>
+      <a href='' className='faction-card hover:shadow-[0px_0px_89px_0px_#C1FF73]'>
+        <div className='faction-blur'>
+          <p>Staynetech</p>
+        </div>
+        <img src={Staynetech} alt="" />
+      </a>
+      <a href='' className='faction-card hover:shadow-[0px_0px_89px_0px_#009E16]'>
+        <div className='faction-blur'>
+          <p>mercenarios</p>
+        </div>
+        <img src={Mercenarios} alt="" />
+      </a>
+    </div>
     </div>
   )
 }
--- src/components/Feactures.jsx
+++ src/components/Feactures.jsx
@@ -19,13 +19,11 @@ const Feactures = () => {
       </div>
       <div className="flex justify-between flex-col-reverse lg:flex-row mt-20 lg:mt-40">
         <div className="w-full lg:w-1/2 lg:mr-10 text-white flex flex-col lg:items-end ">
-          <h2 className="mb-3 font-bold lg:text-[36px] text-3xl">El Último Refugio</h2>
+          <h2 className="mb-3 font-bold lg:text-[36px] text-3xl">Comienzo de la Aventura</h2>
           <p className="mb-10 lg:text-right">
-          En el año 2016, una nave alienígena masiva se estrelló en el viejo continente liberando una letal raza de alienígenas parasitarios que amenazan con devorar el mundo. 
-            <br /> <br />
-            La humanidad se vio obligada a abandonar las ciudades y refugiarse en fortalezas diseñadas para sobrevivir a los devoradores y resistir la embestida de otra raza que viene a cazar a los restos de la humanidad y los devoradores. 
-            <br /><br />
-            ¡Ahora, el destino de la humanidad pende de un hilo mientras se enfrenta a una doble amenaza: la implacable invasión de los alienígenas y la caza implacable proveniente de otro mundo! 
+          En Agoreios, somos apasionados por los juegos de mesa y por ofrecer experiencias únicas y emocionantes a nuestros jugadores. Nuestro equipo está dedicado a crear mundos imaginativos donde la estrategia y la diversión se encuentran.
+          <br /><br />
+          Desde el diseño de miniaturas hasta la elaboración de reglas y escenarios, cada aspecto de Tactical Encounters está cuidadosamente diseñado para brindar una experiencia de juego inolvidable. 
           </p>
           <a href="" className="bg-red-700 hover:bg-red-500 hover:scale-105 w-fit px-6 py-3 rounded-md transition-all duration-300">
             Empieza a jugar
