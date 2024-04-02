2afa9426df6de170812e5efa16d557636a080c04
Bryan <bryan@MacBook-Air-de-Bryan.local>
2024-04-02 14:54:56 -0600

    changes img settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage:{
-        'Hero': "url('src/assets/hero.webp')",
-        'HeroMobile': "url('src/assets/hero-mobile.webp')",
-        'Feactures': "url('src/assets/feactures.webp')"
-      }
     },
   },
   plugins: [],
