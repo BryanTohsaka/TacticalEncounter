a1aba8cc40cf716d6f62ebd971a7ec6806cb39d1
Bryan <bryan@MacBook-Air-de-Bryan.local>
2024-05-02 17:06:37 -0600

    url cover add v2

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -27,7 +27,7 @@ const History = () => {
     };
   
     const loadCover = async () => {
-      const response = await fetch(`https://tactical-encounter-seven.vercel.app/assets/${faction.cover}`);
+      const response = await fetch(`${faction.cover}`);
       const blob = await response.blob();
       setCoverImage(URL.createObjectURL(blob));
     };
